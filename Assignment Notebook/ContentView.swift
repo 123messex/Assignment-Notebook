deedb8cc6278b1eb374cf882f08330d9b58aaa5e
Michael Essex <123messex@gmail.com>
2021-03-05 10:53:28 -0600

    Created "List of Things"

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -7,15 +7,24 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+        struct ContentView: View {
+            var body: some View {
+                NavigationView {
+                List {
+                    Text("First Thing")
+                    Text("Second Thing")
+                    Text("Third Thing")
+                    Text("Fourth Thing")
+                    Text("Fifth Thing")
+                }
+                .navigationBarTitle("Things")
+            }
+        }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
