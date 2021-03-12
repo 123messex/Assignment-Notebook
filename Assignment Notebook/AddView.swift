326801a18f62d46ad6a773f0fde50fcd9327eba0
Michael Essex <123messex@gmail.com>
2021-03-12 10:49:17 -0600

    Finished Code

---
+++ Assignment Notebook/AddView.swift
@@ -0,0 +1,48 @@
+//
+//  AddView.swift
+//  Assignment Notebook
+//
+//  Created by Student on 3/12/21.
+//
+
+import SwiftUI
+struct AddView: View {
+    @ObservedObject var assignmentList: AssignmentList
+    @State private var course = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presentationMode
+    static let courses = ["Studies in lit and film", "Math 101", "Advanced Earth Science", "Mobile App dev",
+                          "Sociology"]
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Course", selection: $course) {
+                    ForEach(Self.courses, id: \.self) { course in
+                        Text(course)
+                    }
+                }
+                TextField("Description", text: $description)
+                DatePicker("Due Date", selection: $dueDate,
+                           
+                           displayedComponents: .date)
+                
+            }
+            .navigationBarTitle("Add New Assignment")
+            .navigationBarItems(trailing: Button("Save") {
+                if course.count > 0 && description.count > 0 {
+                    let item = Assignment(id: UUID(), course: course,
+                                          description: description, dueDate: dueDate)
+                    
+                    assignmentList.items.append(item)
+                    presentationMode.wrappedValue.dismiss()
+                }
+            })
+        }
+    }
+}
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(assignmentList: AssignmentList())
+    }
+}
--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -6,25 +6,56 @@
 //
 
 import SwiftUI
-
-        struct ContentView: View {
-            var body: some View {
-                NavigationView {
-                List {
-                    Text("First Thing")
-                    Text("Second Thing")
-                    Text("Third Thing")
-                    Text("Fourth Thing")
-                    Text("Fifth Thing")
+struct ContentView: View {
+    @ObservedObject var assignmentList = AssignmentList()
+    @State private var showingAddView = false
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(assignmentList.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.course)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        
+                        Text(item.dueDate, style: .date)
+                        
+                    }
                 }
-                .navigationBarTitle("Things")
+                .onMove(perform: { indices, newOffset in
+                    assignmentList.items.move(fromOffsets: indices, toOffset:
+                                                
+                                                newOffset)
+                    
+                })
+                .onDelete(perform: { indexSet in
+                    assignmentList.items.remove(atOffsets: indexSet)
+                })
             }
+            .sheet(isPresented: $showingAddView, content: {
+                AddView(assignmentList: assignmentList)
+            })
+            .navigationBarTitle("Assignment List")
+            .navigationBarItems(leading: EditButton(),
+                                trailing: Button(action: {
+                                                    showingAddView = true}) {
+                                    Image(systemName: "plus")
+                                })
         }
     }
-
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+    
 }
-
+struct Assignment: Identifiable, Codable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
+}
